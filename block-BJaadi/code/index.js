e2af396f95b50455ad0f43a6e2591a6ad7390f23
farhaneas11 <farhan.salam11@gmail.com>
2021-03-20 11:35:37 +0530

    js4

--- block-BJaadi/code/index.js
+++ block-BJaadi/code/index.js
@@ -3,29 +3,29 @@ var numbers = [1, 12, 4, 18, 9, 7, 11, 3, 101, 5, 6, 9];
 var strings = ['This', 'is', 'a', 'collection', 'of', 'words'];
 
 // - Find the index of `101` in numbers
-
+numbers.indexOf(101);
 // - Find the last index of `9` in numbers
-
+numbers.lastIndexOf(9);
 // - Convert value of strings array into a sentance like "This is a collection of words"
-
+strings.join(" ");
 // - Add two new words in the strings array "called" and "sentance"
-
+strings.splice(0,0,"called","sentance");
 // - Again convert the updated array (strings) into sentance like "This is a collection of words called sentance"
-
+strings.join();
 // - Remove the first word in the array (strings)
-
+strings.shift();
 // - Find all the words that contain 'is' use string method 'includes'
-
+strings.includes("is");
 // - Find all the words that contain 'is' use string method 'indexOf'
-
+let findis = strings.filter((string)=> string.indexOf("is"));
 // - Check if all the numbers in numbers array are divisible by three use array method (every)
-
+let div = numbers.every((number)=> number % 3 );
 // -  Sort Array from smallest to largest
-
+let arraysort = numbers.sort((a.b)=> a -b);
 // - Remove the last word in strings
-
+ strings.pop();
 // - Find largest number in numbers
-
+let findLargest = numbers.
 // - Find longest string in strings
 
 // - Find all the even numbers
